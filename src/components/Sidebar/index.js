d566ecd794af15ac9e5929c7cb77b49da7444c0b
ErinNyx <erindanytenyx@gmail.com>
2022-10-25 14:36:49 -0400

    basename added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,10 +17,10 @@ const Sidebar = () => (
             <NavLink exact='true' activeclassname='active' to='/portfolio'>
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e'></FontAwesomeIcon>
             </NavLink>
-            <NavLink exact='true' activeclassname='active' to='/about' className='about-link'>
+            <NavLink exact='true' activeclassname='active' to='/portfolio/about' className='about-link'>
                 <FontAwesomeIcon icon={faUser} color='#4d4d4e'></FontAwesomeIcon>
             </NavLink>
-            <NavLink exact='true' activeclassname='active' to='/contact' className='contact-link'>
+            <NavLink exact='true' activeclassname='active' to='/portfolio/contact' className='contact-link'>
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'></FontAwesomeIcon>
             </NavLink>
         </nav>
